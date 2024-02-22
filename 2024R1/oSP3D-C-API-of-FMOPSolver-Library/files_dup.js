b0cb3ff0f89ecaa380af235fd3ff7833e7f1a427
Paul Coinaud <paul.coinaud@ansys.com>
2024-02-22 14:17:24 +0100

    oSP3D-C-API-of-FMOPSolver-Library

---
+++ 2024R1/oSP3D-C-API-of-FMOPSolver-Library/files_dup.js
@@ -0,0 +1,7 @@
+var files_dup =
+[
+    [ "fmop_solver.h", "fmop__solver_8h.xhtml", "fmop__solver_8h" ],
+    [ "FMOPSolver.h", "_f_m_o_p_solver_8h_source.xhtml", null ],
+    [ "sos_capi_common.h", "sos__capi__common_8h.xhtml", "sos__capi__common_8h" ],
+    [ "sos_capi_script.h", "sos__capi__script_8h.xhtml", "sos__capi__script_8h" ]
+];
