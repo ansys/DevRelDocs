7b7716b309671bb6bccad006e244f51d473643f9
Paul Coinaud <paul.coinaud@ansys.com>
2024-02-22 14:17:24 +0100

    oSP3D-C-API-of-FMOPSolver-Library

---
+++ 2024R1/oSP3D-C-API-of-FMOPSolver-Library/navtreedata.js
@@ -0,0 +1,48 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Statistics on Structures programming library API Documentation", "index.xhtml", [
+    [ "Deprecated List", "deprecated.xhtml", null ],
+    [ "Files", "files.xhtml", [
+      [ "File List", "files.xhtml", "files_dup" ],
+      [ "File Members", "globals.xhtml", [
+        [ "All", "globals.xhtml", null ],
+        [ "Functions", "globals_func.xhtml", null ],
+        [ "Typedefs", "globals_type.xhtml", null ],
+        [ "Enumerations", "globals_enum.xhtml", null ],
+        [ "Enumerator", "globals_eval.xhtml", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_f_m_o_p_solver_8h_source.xhtml"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
